b74f1b8fc174a988522172b6e094588b7bd4f4f1
sreekanth7222 <sreekanthreddy7222@gmail.com>
2025-07-30 20:02:04 +0530

    fifth commit

--- src/components/Home/Calculator/index.js
+++ src/components/Home/Calculator/index.js
@@ -1,65 +1,73 @@
-import React, { useState } from 'react';
+import { Component} from 'react';
+import {evaluate} from 'mathjs'
 import './index.css';
 
-const Calculator = () => {
-  const [display, setDisplay] = useState('0');
+class Calculator extends Component {
+  state = {display:'0'}
 
   // This function handles all button clicks
-  const handleClick = (value) => {
+   handleClick = (value) => {
+    const {display} = this.state
     if (value === 'C') {
       // Clear the display
-      setDisplay('0');
+      this.setState({display:'0'})
     } else if (value === '=') {
       // Evaluate the expression using eval()
       // Note: eval() can be a security risk in other contexts, but is safe here
       // as the input is controlled by our buttons.
       try {
-        // eslint-disable-next-line no-eval
-        setDisplay(eval(display).toString());
+        
+        this.setState(prevState =>({display: evaluate(prevState.display).toString()}));
       } catch {
-        setDisplay('Error');
+        this.setState({display:"Error"});
       }
     } else {
-      // Append the clicked value to the display
-      // If the current display is '0' or 'Error', replace it
+      
       if (display === '0' || display === 'Error') {
-        setDisplay(value);
+        if('0123456789.'.includes(value)){
+          this.setState({display:value})
+        }
       } else {
-        setDisplay(display + value);
+        
+        this.setState(prevState =>({display : prevState.display + value}))
       }
     }
   };
-
+  render(){
+    const {display} = this.state
   return (
     <div className="calculator-container">
+      <h1 className='calculator-heading'>Calculator</h1>
       <div className="calculator">
+        
         <div className="display">{display}</div>
         <div className="buttons">
-          <button onClick={() => handleClick('C')} className="button clear span-two">C</button>
-          <button onClick={() => handleClick('/')} className="button operator">/</button>
-          <button onClick={() => handleClick('*')} className="button operator">*</button>
+          <button onClick={() => this.handleClick('C')} className="button clear span-two">C</button>
+          <button onClick={() => this.handleClick('/')} className="button operator">/</button>
+          <button onClick={() => this.handleClick('*')} className="button operator">*</button>
           
-          <button onClick={() => handleClick('7')} className="button">7</button>
-          <button onClick={() => handleClick('8')} className="button">8</button>
-          <button onClick={() => handleClick('9')} className="button">9</button>
-          <button onClick={() => handleClick('-')} className="button operator">-</button>
+          <button onClick={() => this.handleClick('7')} className="button">7</button>
+          <button onClick={() => this.handleClick('8')} className="button">8</button>
+          <button onClick={() => this.handleClick('9')} className="button">9</button>
+          <button onClick={() => this.handleClick('-')} className="button operator">-</button>
 
-          <button onClick={() => handleClick('4')} className="button">4</button>
-          <button onClick={() => handleClick('5')} className="button">5</button>
-          <button onClick={() => handleClick('6')} className="button">6</button>
-          <button onClick={() => handleClick('+')} className="button operator">+</button>
+          <button onClick={() => this.handleClick('4')} className="button">4</button>
+          <button onClick={() => this.handleClick('5')} className="button">5</button>
+          <button onClick={() => this.handleClick('6')} className="button">6</button>
+          <button onClick={() => this.handleClick('+')} className="button operator">+</button>
 
-          <button onClick={() => handleClick('1')} className="button">1</button>
-          <button onClick={() => handleClick('2')} className="button">2</button>
-          <button onClick={() => handleClick('3')} className="button">3</button>
-          <button onClick={() => handleClick('=')} className="button equals span-two-rows">=</button>
+          <button onClick={() => this.handleClick('1')} className="button">1</button>
+          <button onClick={() => this.handleClick('2')} className="button">2</button>
+          <button onClick={() => this.handleClick('3')} className="button">3</button>
+          <button onClick={() => this.handleClick('=')} className="button equals span-two-rows">=</button>
           
-          <button onClick={() => handleClick('0')} className="button span-two">0</button>
-          <button onClick={() => handleClick('.')} className="button">.</button>
+          <button onClick={() => this.handleClick('0')} className="button span-two">0</button>
+          <button onClick={() => this.handleClick('.')} className="button">.</button>
         </div>
       </div>
     </div>
   );
+}
 };
 
 export default Calculator;
